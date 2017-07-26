e57c92d91bcc0ef31b09b264dec6603658f076a3
Seok Hsu <362288496@qq.com>
2017-07-26 10:02:03 +0800

    test
    尝试上传

---
+++ JitterPhysics/Assets/Demo/Boxes/GameSyncManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using TrueSync;
+
+/**
+* @brief Manages boxes instantiation.
+**/
+public class GameSyncManager : TrueSyncBehaviour {
+    
+    /**
+    * @brief Initial setup when game is started.
+    **/
+    public override void OnSyncedStart() {
+        
+    }
+
+    /**
+    * @brief Logs a text when game is paused.
+    **/
+    public override void OnGamePaused() {
+        Debug.Log("Game Paused");
+    }
+
+    /**
+    * @brief Logs a text when game is unpaused.
+    **/
+    public override void OnGameUnPaused() {
+        Debug.Log("Game UnPaused");
+    }
+
+    /**
+    * @brief Logs a text when game is ended.
+    **/
+    public override void OnGameEnded() {
+        Debug.Log("Game Ended");
+    }
+
+    /**
+    * @brief When a player get disconnected all objects belonging to him are destroyed.
+    **/
+    public override void OnPlayerDisconnection(int playerId) {
+        TrueSyncManager.RemovePlayer(playerId);
+    }
+
+}
---
+++ JitterPhysics/Assets/TrueSync/Unity/TSHingeJoint.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+using TrueSync.Physics3D;
+
+namespace TrueSync
+{
+    [Serializable]
+    public class SpringElements
+    {
+        public FP spring;
+        public FP damper;
+        public FP tagetPosition;
+    }
+
+    [Serializable]
+    public class LimitElements
+    {
+        public FP Min;
+        public FP Max;
+        //TODO: Bouciness, Bounce Mi Vel, Contact Distance...
+    }
+
+
+    public class TSHingeJoint : TrueSyncBehaviour
+    {
+        HingeJoint3D thisJoint;
+        TSRigidBody thisBody;
+
+
+        [SerializeField]
+        TSCollider connectedBody;
+        [SerializeField]
+        Vector3 anchor;
+        [SerializeField]
+        Vector3 Axis;
+        //[SerializeField]
+        //bool useLimits = false;
+        [SerializeField]
+        LimitElements Limits;
+        [SerializeField]
+        bool useSpring;
+        [SerializeField]
+        SpringElements Spring;
+        [SerializeField]
+        FP breakForce = FP.PositiveInfinity;
+        TSVector TSWorldAxis;
+
+
+
+        public override void OnSyncedStart()
+        {
+            thisBody = GetComponent<TSRigidBody>();
+            IBody3D body1 = GetComponent<TSCollider>().Body;
+            IBody3D body2 = connectedBody.Body;
+
+            Vector3 worldPos = transform.TransformPoint(anchor);
+            TSVector TSworldPos = worldPos.ToTSVector();
+
+            Vector3 worldAxis = transform.TransformDirection(Axis);
+            TSWorldAxis = worldAxis.ToTSVector();
+
+
+            //if (useLimits)
+            //    thisJoint = new LimitedHingeJoint(PhysicsWorldManager.instance.GetWorld(), body1, body2, TSworldPos, TSWorldAxis, -Limits.Min, Limits.Max);
+            //else
+                thisJoint = new HingeJoint3D(PhysicsWorldManager.instance.GetWorld(), body1, body2, TSworldPos, TSWorldAxis);
+           
+        }
+
+        public override void OnSyncedUpdate()
+        {
+            if (useSpring)
+            {
+                //Adding a spring and damper Term to the Equation of Motion 
+                thisBody.AddTorque((-1) * TSWorldAxis * ((thisJoint.getHingeAngle() - Spring.tagetPosition) * Spring.spring + thisJoint.getAngularVel() * Spring.damper));
+            }
+
+            if (TSMath.Abs(thisJoint.AppliedImpulse) >= breakForce)//@TODO: Add break torque
+            {
+                thisJoint.Deactivate();
+                Destroy(this);
+            }
+
+        }
+
+    }
+}
